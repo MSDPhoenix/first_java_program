fc20fe49c452d5f471aba0a2b1f995c0765d4efd
Michael S DuWors <46774296+MSDPhoenix@users.noreply.github.com>
2023-04-30 21:24:14 -0700

    first java repo

---
+++ first_java_program.java
@@ -0,0 +1,8 @@
+public class first_java_program {
+    public static void main(String[] args) {
+        System.out.println("My name is Michael DuWors");
+        System.out.println("I am 49 years old");
+        System.out.println("My hometown is Seattle, WA");
+    }
+
+}
